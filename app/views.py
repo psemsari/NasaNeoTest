43958ea49fd24e4cccc2ce48f68127ce26794703
psemsari <psemsari@student.42.fr>
2022-11-09 18:20:02 +0100

    testing of nasa api with a simple form (no data validation yet)

--- app/views.py
+++ app/views.py
@@ -7,12 +7,21 @@ from django import forms
 class DatesNeo(forms.Form):
     start = forms.DateField()
     end = forms.DateField()
-# Create your views here.
+
 def index(request: HttpRequest):
-    start = '2022-11-08'
-    end = '2022-11-09'
-    Huston: Response = get('https://api.nasa.gov/neo/rest/v1/feed?start_date=' + start + '&end_date=' + end + '&api_key=' + settings.SECRET_NASA)
-    obj = Huston.json()
-    for x in obj['near_earth_object'][start]:
-        print(x['name'], x['id'])
-    return (HttpResponse(Huston.content, content_type="application/json"))
+    """Return Test of the nasa api"""
+    if request.method == 'GET':
+        start = '2022-11-08'
+        end = '2022-11-09'
+        Huston: Response = get('https://api.nasa.gov/neo/rest/v1/feed?start_date=' + start + '&end_date=' + end + '&api_key=' + settings.SECRET_NASA)
+        obj = Huston.json()
+        for x in obj['near_earth_objects'][start]:
+            print(x['name'], x['id'])
+        return (render(request, 'base.html', {'form': DatesNeo()} ))#HttpResponse(Huston.content, content_type="application/json")
+    if request.method == 'POST':
+        form = DatesNeo(request.POST)
+        start = form.data['start']
+        Huston: Response = get('https://api.nasa.gov/neo/rest/v1/feed?start_date=' + start + '&end_date=' + form.data['end'] + '&api_key=' + settings.SECRET_NASA)
+        obj = Huston.json()
+        dico = obj['near_earth_objects']
+        return (render(request, 'base.html', {'form': DatesNeo(), 'obj': dico} ))
